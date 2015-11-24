6d414f32dd7e848c4ad6506e0e1644bfaaf68ecc
Raul I <thefloyd2112@gmail.com>
2015-11-24 15:23:36 -0800

    Working on landing page

--- project/apps/turkeykill/urls.py
+++ project/apps/turkeykill/urls.py
@@ -1,1 +1,10 @@
-urls.py
+from django.conf.urls import patterns, url
+from apps.turkeykill import views
+urlpatterns = patterns('',
+	# url(r'^process/$', views.process, name="process"),
+	# url(r'^update/(?P<product_id>\d+)/$', views.update, name="update"),
+	# url(r'^delete/(?P<product_id>\d+)/$', views.delete, name="delete"),
+	# url(r'^create/$', views.create, name= 'create'),
+	# url(r'^show/(?P<product_id>\d+)/$', views.show, name="show"),
+	url(r'^$', views.index, name='index'),
+)
--- project/apps/turkeykill/views.py
+++ project/apps/turkeykill/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
+import random
+from datetime import datetime
+time = datetime.now()
 
-# Create your views here.
+def index(request):
+	try:
+		request.session['gold']
+	except:
+		request.session['gold']=0
+	try:
+		request.session['mymessage']
+	except:
+		request.session['mymessage'] = []
+	return render(request, 'turkeykill/index.html')
+def attack_turkey(request):
+	if request.POST['action'] == 'attack_turkey':
+		request.session['damage'] = random.randrange(1,5)
+		request.session['hitpoints'] -= request.session['damage']
+		result= "Dealt" + str(request.session['damage']) + "to the Turkenator" + str(time)
+		request.session['mymessage'].insert(0,str(result))
+	return redirect('/')
